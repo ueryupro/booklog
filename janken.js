6368e4cca9b2781036d205806b1b7fc4ed14b095
上田竜次 <uedaryuji@uedaryuujinoMacBook-Pro.local>
2022-01-13 00:20:08 +0900

    feat:janken buttle conditional branch

--- janken.js
+++ janken.js
@@ -21,22 +21,39 @@ app.use(express.json());
 
 app.get("/janken", (req, res) => {
   const hand = req.body.hand;
-  let userHand = undefined;
+  let yourHand;
   if (hand === "グー") {
-    userHand = 0;
+    yourHand = 0;
   } else if (hand === "チョキ") {
-    userHand = 1;
+    yourHand = 1;
   } else if (hand === "パー") {
-    userHand = 2;
+    yourHand = 2;
   } else {
-    userHand = "グー、チョキ、パーのいずれかを入力してください";
+    yourHand = "グー、チョキ、パーのいずれかを入力してください";
   }
 
   const cpHand = Math.floor(Math.random() * 3);
+  let cp;
+  if (cpHand === 0) {
+    cp = "グー";
+  } else if (cpHand === 1) {
+    cp = "チョキ";
+  } else if (cpHand === 2) {
+    cp = "パー";
+  }
+
+  let result;
+  if ((yourHand === 0 && cpHand === 0) || (yourHand === 1 && cpHand === 1) || (yourHand === 2 && cpHand === 2)) {
+    result = "あいこ";
+  } else if ((yourHand === 0 && cpHand === 2) || (yourHand === 1 && cpHand === 0) || (yourHand === 2 && cpHand === 1)) {
+    result = "あなたの負け";
+  } else if ((yourHand === 0 && cpHand === 1) || (yourHand === 1 && cpHand === 2) || (yourHand === 2 && cpHand === 0)) {
+    result = "あなたの勝ち";
+  }
   res.json({
-    "ok": true,
-    "your-hand": userHand,
-    "cp-hand": cpHand
+    "your-hand": hand,
+    "cp-hand": cp,
+    "result": result
   })
 })
 
