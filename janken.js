a170b475b76eea49336b98ed790a3945be9cbb18
上田竜次 <uedaryuji@uedaryuujinoMacBook-Pro.local>
2022-01-13 00:21:48 +0900

    refactor; delete nouse post http request

--- janken.js
+++ janken.js
@@ -4,21 +4,6 @@ const port = 3000;
 
 app.use(express.json());
 
-// app.post('/booklog', (req, res) => {
-//   booklog = req.body;
-
-//   if (!(booklog.name && booklog.text)) {
-//     return res.json({
-//       "ok": false,
-//       "error": "invalid parameter"
-//     })
-//   }
-//   res.json({
-//     "ok": true,
-//     "booklog": booklog
-//   })
-// })
-
 app.get("/janken", (req, res) => {
   const hand = req.body.hand;
   let yourHand;
